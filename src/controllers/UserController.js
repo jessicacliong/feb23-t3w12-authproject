bedbadc83bd462bb6a30456c8257285a4dc0a3b2
jessicacliong <14395@coderacademy.edu.au>
2023-11-07 20:53:50 +1100

    feat: login route implemented

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,8 @@
 // Import Express Library
 const express = require('express');
+const { User } = require('../models/UserModel');
+const { comparePassword } = require('../functions/userAuthFunctions');
+
 
 // mkae an instance of a Router
 const router = express.Router();
@@ -50,8 +53,25 @@ router.get("/id", async (request, response) => {
 // request.body = {username: "admin", password: "Password1"}
 // respond with {jwt:"aciuhwejnasdiuhawecuaciuh"}
 router.post("/login", async (request, response) => {
+	// Find user by provided username 
+	let targetUser = await User.findOne({username: request.body.username}).catch(error => error);
 
-})
+	// Check if user provided the correct password
+	let isPasswordCorrect = await comparePassword(request.body.password, targetUser.password);
+
+	if (!isPasswordCorrect) {
+		response.status(403).json({error:"You are not authorised to do this!"});
+	}
+
+	// If they provided the correct, generate a JWT
+	let freshJwt = generateJwt(targetUser._id.tostring());
+
+	// respond with the JWT 
+	respond.json({
+		jwt: freshJwt
+	});
+
+});
 
 // POST localhost:3000/users/verify
 // JWT in request.headers["jwt"] or request.headers["authorization"]
